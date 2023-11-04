d6bad441170b0cd44e66606f6556fd24f2ae4ea9
mehdinathani <mehdinathani@gmail.com>
2023-11-04 14:51:51 +0500

    dart theme demo

---
+++ lib/sharedpref_functions.dart
@@ -0,0 +1,12 @@
+// import 'package:shared_preferences/shared_preferences.dart';
+
+// Future<void> saveThemeMode(String themeMode) async {
+//   final prefs = await SharedPreferences.getInstance();
+//   prefs.setString('theme_mode', themeMode);
+// }
+
+// Future<String> loadThemeMode() async {
+//   final prefs = await SharedPreferences.getInstance();
+//   return prefs.getString('theme_mode') ??
+//       'light'; // Default to 'light' if not set
+// }
